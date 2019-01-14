ab654c3260e2d9232b63956aa07379f8eb96ddb1
EkkoC <46436307+EkkoC@users.noreply.github.com>
2019-01-14 17:22:40 +0800

    Add files via upload

---
+++ SQLConnection/SQLConn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Webangular.SQLConnection
+{
+    public class SQLConn
+    {
+        public IDbConnection CreateConnection(string name = "default")
+        {
+            switch (name)
+            {
+                case "default":
+                    {
+                        var ConnectionString = System.Web.Configuration.WebConfigurationManager.ConnectionStrings["Connstr"].ConnectionString;
+
+                        return new SqlConnection(ConnectionString);
+                    }
+                default:
+                    {
+                        throw new Exception("name 不存在。");
+                    }
+            }
+        }
+    }
+}
---
+++ ScriptsNg/Controller/TestCtrl.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("myapp", []);
+app.controller("TestCtrl", ['$scope', '$http',function ($scope, $http) {
+    $scope.Message = "123";
+
+
+    $http.get("/api/Values").then(function (response) {
+        $scope.x = response.data;
+    })
+
+    //$http.get("api/TestController/Test123").success(function (data) {
+    //    $scope.xx = data;
+    //})
+
+
+}]);
